d148b520cc7f6296eff9a059964da50c81816db0
N-ckDotCom <lis20312@spengergasse.at>
2023-03-17 14:25:02 +0100

    Fixed wrong index

--- portfolio/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.ae8ce0e1.js';
-export const imports = ["_app/immutable/entry/error.svelte.ae8ce0e1.js","_app/immutable/chunks/index.4672743b.js","_app/immutable/chunks/singletons.c704fee7.js"];
-export const stylesheets = [];
-export const fonts = [];
--- portfolio/build/_app/immutable/chunks/1.010d994e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.ae8ce0e1.js";export{t as component};
